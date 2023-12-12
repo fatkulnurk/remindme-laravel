eda5e227c3f17b721257715218027604c95f36f8
fatkulnurk <fatkulnurk@gmail.com>
2023-12-12 21:41:46 +0700

    create unit test for enum TokenAbilityTest.php

---
+++ src/tests/Unit/Enums/Tokens/TokenAbilityTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Enums\Tokens;
+
+use App\Enums\Tokens\TokenAbility;
+use Illuminate\Foundation\Testing\TestCase;
+use Tests\CreatesApplication;
+
+/**
+ * @package Tests\Unit\Services
+ * @author  Fatkul Nur Koirudin <https://github.com/fatkulnurk>
+ * */
+class TokenAbilityTest extends TestCase
+{
+    use CreatesApplication;
+
+    public function test_get_values_as_array()
+    {
+        $cases = TokenAbility::cases();
+        $this->assertIsArray($cases);
+    }
+
+    public function test_get_value()
+    {
+        $tokenName = TokenAbility::SESSION_REFRESH;
+        $this->assertEquals('session:refresh', $tokenName->value);
+    }
+
+    public function test_get_name()
+    {
+        $tokenName = TokenAbility::SESSION_REFRESH;
+        $this->assertEquals(TokenAbility::SESSION_REFRESH->name, $tokenName->name);
+    }
+}
